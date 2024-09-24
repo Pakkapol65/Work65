d020fcd670c57ce984bea61f9287ccf24aae8c4a
unknown <655021001277@mail.rmutk.ac.th>
2024-09-24 13:40:28 +0700

    first commit

---
+++ New folder/index.js
@@ -0,0 +1,8 @@
+
+function changetittle(){
+    document.getElementById('tittle').innerHTML = 'Google Page';
+}
+function cal(){
+    let r = parseFloat(document.getElementById('txt').value);
+    document.getElementById('resule').innerHTML = 3.14159*r*r;
+}
